9191a7cea8ffe2dcec1139c5178e5eac09d30f43
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-27 10:24:57 +0200

    g8/added index - action|submit - server draft

---
+++ serve.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+)
+
+func main () {
+
+
+    // 3 routes
+    //  - /         ---> index.html
+    //  - /hell     - hello world
+    //  - /action   - consume action ---> action.html
+
+
+
+}
+
